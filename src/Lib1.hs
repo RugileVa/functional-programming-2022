5a96478b756d00f98b6fe52f294c0c144f2156cd
henlo <48015670+dominykase@users.noreply.github.com>
2022-09-11 18:27:01 +0300

    created Square data structure that cna be JSON encoded

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -1,32 +1,50 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Lib1(
     State, emptyState, gameStart, render, mkCheck, toggle, hint
 ) where
 
 import Types
+import Data.Aeson
+import Data.ByteString.Lazy.Char8 (unpack)
+import GHC.Generics (Generic)
+import Data.ByteString.Lazy (pack)
 
 -- This is a state of your game.
 -- It must contain all values you might need during a game:
 -- number of occupied rows/cols, hints, occupied cells,..
 -- You can change the right hand side as you wish but please
 -- keep the type name as is
-data State = State [String]
+data State = State [String] 
     deriving Show
+
+data Square = Square {
+    coord :: Coord,
+    chr :: Char
+} deriving (Generic, Show, Eq)
+instance ToJSON Square
 
 -- IMPLEMENT
 -- This is very initial state of your program
 emptyState :: State
-emptyState = State ["Initial state"]
-
+emptyState = State [unpack (encode [Square {coord = Coord {col = 1, row = 1}, chr = ' '}, Square {coord = Coord {col = 2, row = 1}, chr = ' '}, Square {coord = Coord {col = 3, row = 1}, chr = ' '}])]
+-------------------- -------------------------------
 -- IMPLEMENT
 -- This adds game data to initial state 
 gameStart :: State -> Document -> State
 gameStart (State l) d = State $ ("Game started: " ++ show d) : l
 
+-- gets the last element of a list
+listLast :: [a] -> a
+listLast [x] = x --base case is when there's just one element remaining
+listLast (_:xs) = listLast xs --if there's anything in the head, continue until there's one element left
+listLast [] = error "Can't do last of an empty list!"
+
 -- IMPLEMENT
 -- renders your game board
 render :: State -> String
-render = show
+render (State l) = do
+    show (listLast l)
 
 -- IMPLEMENT
 -- Make check from current state
